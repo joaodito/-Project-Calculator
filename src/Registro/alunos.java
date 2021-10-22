26f42172dcf00a192dedc2e4abfb83a74e6d2668
joaodito <58365406+joaodito@users.noreply.github.com>
2021-10-21 23:20:42 -0300

    Update alunos.java

--- src/Registro/alunos.java
+++ src/Registro/alunos.java
@@ -1,56 +1,26 @@
 package Registro;
 
-import javax.swing.JOptionPane;
-
 public class aluno {
-
 		private String nomeAluno;
 		private String emailAluno;
-		private float totalDeRendimento;
-		private int numeroDeAlunos;
-		private float rendimentoDoAluno;
+		private String totalDeRendimentoAluno;
 
 
-		public aluno (String nomeAluno, String emailAluno, float totalDeRendimentos) {
-			this.nomeAluno = nomeAluno;
-			this.emailAluno = emailAluno;
-			this.totalDeRendimento = totalDeRendimentos;		
+		public aluno(String nome, String email, String rendimentos) {
+			this.nomeAluno = nome;
+			this.emailAluno = email;
+			this.totalDeRendimentoAluno = rendimentos;		
 			}
 
-		public String getNomeAluno() {
-			return nomeAluno;
+		public String getRendimentos() {
+			return totalDeRendimentoAluno;
 		}
-
-		public String getEmailAluno() {
-			return emailAluno;
-		}
-
-		public float getTotalDeRendimentoAluno() {
-			return getTotalDeRendimentoAluno();
-		}
-
-		public int getNumeroDeAlunos() {
-			return numeroDeAlunos;
-		}
-
-		public void CadastrarAluno() {
-			Aluno [] alunos = new Aluno [0];
-			nomeAluno = JOptionPane.showInputDialog("Informe o nome do aluno");
-			emailAluno = JOptionPane.showInputDialog("Informe o e-mail do aluno");
-			rendimentoDoAluno = Float.parseFloat(JOptionPane.showInputDialog("Informe o rendimento do aluno"));
-			totalDeRendimento = rendimentoDoAluno + totalDeRendimento;
-			numeroDeAlunos = numeroDeAlunos+1;
-			Aluno a = new Aluno(nomeAluno,emailAluno, totalDeRendimento);
-
-			Aluno [] tempP = new Aluno [alunos.length+1];
-			for (int i=0; i<alunos.length; i++) {
-				tempP [i] = alunos[i];
-			}
-
-			tempP[alunos.length] = a;
-
-			alunos = tempP;
-
+		public String mostrar() {
+			String resposta = "Aluno:\n"
+	@@ -21,5 +44,25 @@ public String mostrar() {
+							+ "Email: " + emailAluno + "\n"
+							+ "Rendimentos mensais: " + totalDeRendimentoAluno + "\n";
+			return resposta;
 		}
 }
 
