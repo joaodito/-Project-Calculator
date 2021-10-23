321405fdc899bef6a87e06a89d86c441c119866b
Matheus Morinishi <morinishi1512@gmail.com]>
2021-10-23 00:11:20 -0300

    Implementação de algumas classes

--- src/Registro/alunos.java
+++ src/Registro/alunos.java
@@ -1,25 +1,71 @@
 package Registro;
+
+import javax.swing.JOptionPane;
+
 public class alunos {
+
 		private String nomeAluno;
 		private String emailAluno;
-		private String totalDeRendimentoAluno;
-		
-			
-		public alunos(String nome, String email, String rendimentos) {
-			this.nomeAluno = nome;
-			this.emailAluno = email;
-			this.totalDeRendimentoAluno = rendimentos;		
+		private String totalDeRendimento;
+		private int numeroDeAlunos;
+		private float rendimentoDoAluno;
+
+
+		public alunos (String nomeAluno, String emailAluno, String totalDeRendimentos) {
+			this.nomeAluno = nomeAluno;
+			this.emailAluno = emailAluno;
+			this.totalDeRendimento = totalDeRendimentos;		
 			}
 
-		public String getRendimentos() {
-			return totalDeRendimentoAluno;
+
+		public String getNomeAluno() {
+			return nomeAluno;
 		}
-		public String mostrar() {
-			String resposta = "Aluno:\n"
-							+ "------\n"
-							+ "Nome: " + nomeAluno + "\n"
-							+ "Email: " + emailAluno + "\n"
-							+ "Rendimentos mensais: " + totalDeRendimentoAluno + "\n";
-			return resposta;
+
+
+		public void setNomeAluno(String nomeAluno) {
+			this.nomeAluno = nomeAluno;
 		}
-}
+
+
+		public String getEmailAluno() {
+			return emailAluno;
+		}
+
+
+		public void setEmailAluno(String emailAluno) {
+			this.emailAluno = emailAluno;
+		}
+
+
+		public String getTotalDeRendimento() {
+			return totalDeRendimento;
+		}
+
+
+		public void setTotalDeRendimento(String totalDeRendimento) {
+			this.totalDeRendimento = totalDeRendimento;
+		}
+
+
+		public int getNumeroDeAlunos() {
+			return numeroDeAlunos;
+		}
+
+
+		public void setNumeroDeAlunos(int numeroDeAlunos) {
+			this.numeroDeAlunos = numeroDeAlunos;
+		}
+
+
+		public float getRendimentoDoAluno() {
+			return rendimentoDoAluno;
+		}
+
+
+		public void setRendimentoDoAluno(float rendimentoDoAluno) {
+			this.rendimentoDoAluno = rendimentoDoAluno;
+		}
+
+
+	}
