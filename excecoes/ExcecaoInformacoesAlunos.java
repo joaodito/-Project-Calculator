b55a552c39d9006e7a86451287858b53ad11b768
joaodito <58365406+joaodito@users.noreply.github.com>
2021-11-01 22:38:14 -0300

    Create ExcecaoInformacoesAlunos.java

---
+++ excecoes/ExcecaoInformacoesAlunos.java
@@ -0,0 +1,13 @@
+package excecoes;
+
+public class ExcecaoInformacoesAlunos extends RuntimeException{
+
+	public ExcecaoInformacoesAlunos () {
+		super();
+	}
+	
+	public ExcecaoInformacoesAlunos (String msg) {
+		super(msg);
+	}	
+	
+}
